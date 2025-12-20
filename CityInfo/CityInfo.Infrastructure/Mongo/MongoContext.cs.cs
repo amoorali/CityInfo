d410a8b3d3858aed539f7f085a8daf31ed21e2ab
amoorali <amirfallahi222@gmail.com>
2025-12-20 10:24:52 +0330

    added mongodb settings to the project

---
+++ CityInfo/CityInfo.Infrastructure/Mongo/MongoContext.cs.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace CityInfo.Infrastructure.Mongo
+{
+    public sealed class MongoContext
+    {
+        public IMongoDatabase Database { get; }
+
+        public MongoContext(MongoDbSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            Database = client.GetDatabase(settings.DatabaseName);
+        }
+    }
+}
---
+++ CityInfo/CityInfo.Infrastructure/Mongo/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace CityInfo.Infrastructure.Mongo
+{
+    public sealed class MongoDbSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string ReviewsCollectionName { get; set; } = "Reviews";
+    }
+}
