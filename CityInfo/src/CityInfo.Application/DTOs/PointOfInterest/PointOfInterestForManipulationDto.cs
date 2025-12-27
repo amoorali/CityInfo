1b9d1c8ab93d7388f147fdd0a93712cac164ae1d
amoorali <amirfallahi222@gmail.com>
2025-12-27 11:58:11 +0330

    edited max length attribute

--- CityInfo/src/CityInfo.Application/DTOs/PointOfInterest/PointOfInterestForManipulationDto.cs
+++ CityInfo/src/CityInfo.Application/DTOs/PointOfInterest/PointOfInterestForManipulationDto.cs
@@ -9,7 +9,7 @@ namespace CityInfo.Application.DTOs.PointOfInterest
         [MaxLength(100, ErrorMessage = "The name shouldn't have more than 100 characters.")]
         public string Name { get; set; } = string.Empty;
 
-        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters."))]
+        [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
         public virtual string? Description { get; set; }
         #endregion
     }
